036f7dab1266908a407a09e3d09991f74bfefcbe
codelabite <chukwusahpeter@gmail.com>
2021-03-02 13:24:54 +0100

    the start

---
+++ src/base.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+export const app = firebase.initializeApp({
+  apiKey: "AIzaSyD_jYcvgjMhny1DTkjmHTmZLbVGcuMmnqw",
+  authDomain: "lots-charity.firebaseapp.com",
+  projectId: "lots-charity",
+  storageBucket: "lots-charity.appspot.com",
+  messagingSenderId: "56230134298",
+  appId: "1:56230134298:web:0500d9ee3d8546e9afc7a3",
+});
