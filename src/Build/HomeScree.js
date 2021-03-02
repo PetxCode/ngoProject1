783e5d228fcd31df0b25827967905e30905886ca
codelabite <chukwusahpeter@gmail.com>
2021-03-02 14:45:23 +0100

    footer

--- src/Build/HomeScree.js
+++ src/Build/HomeScree.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "./Footer";
 
 const HomeScreen = () => {
   return (
     <div>
       <center>
         <div>Home</div>
+        <Footer />
       </center>
     </div>
   );
