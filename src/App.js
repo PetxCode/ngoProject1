c8649fdcdb5a5f99d9f244104886b6f75ee45c75
codelabite <chukwusahpeter@gmail.com>
2021-03-02 17:43:33 +0100

    final push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "antd/dist/antd.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomeScreen from "./Build/HomeScree";
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <Router>
         <Switch>
           <Route exact path="/" component={HomeScreen} />
