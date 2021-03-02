de985664c64c126aa48833f00731a3ebf85d0a86
codelabite <chukwusahpeter@gmail.com>
2021-03-02 13:50:54 +0100

    new add up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "antd/dist/antd.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
